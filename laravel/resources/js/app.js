33ee7f6df09a9e0de3df80f01fb24337e599e6c3
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2020-07-19 18:31:06 +0900

    カテゴリー機能実装完了

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -8,6 +8,9 @@ import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css' 
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import SocialSharing from 'vue-social-sharing';
+import axios from 'axios'
+
+Vue.prototype.$http = axios;
 
 Vue.use(BootstrapVue)
 Vue.use(SocialSharing);
--- laravel/resources/js/components/page/home.vue
+++ laravel/resources/js/components/page/home.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+
     <the-header></the-header>
     
     <main>
@@ -16,31 +17,16 @@
             <h2 class="home-quiz__setting-h2">
               <img class="home-quiz__setting-h2-logo" src="/images/directory-icon.png" />出題設定
             </h2>
-            <form action="/quiz" method="post">
-              <label>
-                <input type="checkbox" name="categories[]" value="1" checked />ビジネスマナー
-              </label>
-              <label>
-                <input type="checkbox" name="categories[]" value="2" />一般常識
-              </label>
-              <label>
-                <input type="checkbox" name="categories[]" value="3" />就職・転職
-              </label>
-              <label>
-                <input type="checkbox" name="categories[]" value="4" />法律
-              </label>
-              <label>
-                <input type="checkbox" name="categories[]" value="5" />IT
-              </label>
-              <label>
-                <input type="checkbox" name="categories[]" value="6" />雑学
+            <form>
+              <label v-for="(cate, index) in category" :key="index">
+                <input type="checkbox" v-model="categories" :value="cate.id" />{{ cate.name }} 
               </label>
               <div class>
                 全項目チェック
                 <button type="button" name="check_all" id="check-all" value="1">ON</button>
                 <button type="button" name="check_all_off" id="check-all-off" value="1">OFF</button>
               </div>
-              <button type="submit" class="btn btn-primary">出題開始</button>
+              <button type="submit" class="btn btn-primary" @click.stop.prevent="goQuiz()">出題開始</button>
               <input type="hidden" name="_token" value />
             </form>
           </section>
@@ -67,9 +53,9 @@
             <h2 class="home__notice-h2">
               <img class="home__notice-h2-logo" src="/images/news-icon.png" />お知らせ情報
             </h2>
-            <dl>
-              <dt>2019/08/23</dt>
-              <dd>サイトを開設しました。</dd>
+            <dl v-for="(info, index) in information" :key="index">
+              <dt>{{ info.created_at }}</dt>
+              <dd>{{ info.information }}</dd>
             </dl>
           </section>
         </article>
@@ -96,6 +82,26 @@ export default {
     TheFooter,
     TheSidebar,
     BarChart,
+  },
+  data() {
+    return {
+      information :[],
+      category: [],
+      categories: [1]
+    };
+  },
+  mounted() {
+    this.$http.get("/api/category").then(response => {
+      this.category = response.data;
+    });
+    this.$http.get("/api/information").then(response => {
+      this.information = response.data;
+    });
+  },
+  methods: {
+    goQuiz() {
+      this.$router.push("/quiz?categories=" + this.categories);
+    }
   }
 };
 </script>
